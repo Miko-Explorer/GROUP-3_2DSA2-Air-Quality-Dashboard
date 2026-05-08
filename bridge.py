e141bbaf6f5c439f4a68388f2896456e3c773af7
Enrico Miguel Veloso <enrico.veloso1605@gmail.com>
2026-05-08 23:02:34 +0800

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -43,7 +43,9 @@ def run_automation():
                  "San Juan": "https://www.accuweather.com/en/ph/san-juan/264882/air-quality-index/264882",
                  "Valenzuela": "https://www.accuweather.com/en/ph/valenzuela/3424474/air-quality-index/3424474"}
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com/"}
     cities = {}
 
     # Scrape AccuWeather:
