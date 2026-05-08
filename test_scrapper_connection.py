058a7f8e91ae6e28ba84422f62e15767e236892a
Enrico Miguel Veloso <enrico.veloso1605@gmail.com>
2026-05-08 23:03:05 +0800

    Update test_scrapper_connection.py

--- test_scrapper_connection.py
+++ test_scrapper_connection.py
@@ -29,8 +29,7 @@ urls_accu = {"Marikina": "https://www.accuweather.com/en/ph/marikina-heights/170
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://www.google.com/"
-}
+    "Referer": "https://www.google.com/"}
 
 cities = {}
 print("Starting scrape...")
