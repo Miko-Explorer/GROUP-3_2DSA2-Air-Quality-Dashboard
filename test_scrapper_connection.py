b6b9d04a88820478c661f4c99d4fe6715d823b61
Benedict Caliba <benedict.caliba.sci@ust.edu.ph>
2026-05-09 22:20:50 +0800

    Refactor scraping and Google Sheets connection logic

--- test_scrapper_connection.py
+++ test_scrapper_connection.py
@@ -11,28 +11,26 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Data are from AccuWeather
 urls = {"Caloocan": "https://www.accuweather.com/en/ph/caloocan/264875/air-quality-index/264875",
-    "Las Piñas":"https://www.accuweather.com/en/ph/las-pi%C3%B1as/264877/air-quality-index/264877",
-    "Makati":"https://www.accuweather.com/en/ph/makati-city/21-264878_1_al/air-quality-index/21-264878_1_al",
-    "Malabon": "https://www.accuweather.com/en/ph/san-roque/761333/air-quality-index/761333",
-    "Mandaluyong": "https://www.accuweather.com/en/ph/mandaluyong/768148/air-quality-index/768148",
-    "Manila": "https://www.accuweather.com/en/ph/manila/264885/air-quality-index/264885",
-    "Marikina": "https://www.accuweather.com/en/ph/marikina-heights/1707180/air-quality-index/1707180",
-    "Muntinlupa": "https://www.accuweather.com/en/ph/muntinlupa/264879/air-quality-index/264879",
-    "Navotas": "https://www.accuweather.com/en/ph/navotas/765956/air-quality-index/765956",
-    "Parañaque": "https://www.accuweather.com/en/ph/don-bosco/3424484/air-quality-index/3424484",
-    "Pasay": "https://www.accuweather.com/en/ph/pasay-city/2-264881_1_al/air-quality-index/2-264881_1_al",
-    "Pasig": "https://www.accuweather.com/en/ph/pasig/264876/air-quality-index/264876",
-    "Pateros": "https://www.accuweather.com/en/ph/pateros/764136/air-quality-index/764136",
-    "Quezon City": "https://www.accuweather.com/en/ph/quezon-city/264873/air-quality-index/264873",
-    "San Juan": "https://www.accuweather.com/en/ph/san-juan/264882/air-quality-index/264882",
-    "Taguig": "https://www.accuweather.com/en/ph/taguig/759349/air-quality-index/759349",
-    "Valenzuela": "https://www.accuweather.com/en/ph/valenzuela/3424474/air-quality-index/3424474"}
+        "Las Piñas":"https://www.accuweather.com/en/ph/las-pi%C3%B1as/264877/air-quality-index/264877",
+        "Makati":"https://www.accuweather.com/en/ph/makati-city/21-264878_1_al/air-quality-index/21-264878_1_al",
+        "Malabon": "https://www.accuweather.com/en/ph/san-roque/761333/air-quality-index/761333",
+        "Mandaluyong": "https://www.accuweather.com/en/ph/mandaluyong/768148/air-quality-index/768148",
+        "Manila": "https://www.accuweather.com/en/ph/manila/264885/air-quality-index/264885",
+        "Marikina": "https://www.accuweather.com/en/ph/marikina-heights/1707180/air-quality-index/1707180",
+        "Muntinlupa": "https://www.accuweather.com/en/ph/muntinlupa/264879/air-quality-index/264879",
+        "Navotas": "https://www.accuweather.com/en/ph/navotas/765956/air-quality-index/765956",
+        "Parañaque": "https://www.accuweather.com/en/ph/don-bosco/3424484/air-quality-index/3424484",
+        "Pasay": "https://www.accuweather.com/en/ph/pasay-city/2-264881_1_al/air-quality-index/2-264881_1_al",
+        "Pasig": "https://www.accuweather.com/en/ph/pasig/264876/air-quality-index/264876",
+        "Pateros": "https://www.accuweather.com/en/ph/pateros/764136/air-quality-index/764136",
+        "Quezon City": "https://www.accuweather.com/en/ph/quezon-city/264873/air-quality-index/264873",
+        "San Juan": "https://www.accuweather.com/en/ph/san-juan/264882/air-quality-index/264882",
+        "Taguig": "https://www.accuweather.com/en/ph/taguig/759349/air-quality-index/759349",
+        "Valenzuela": "https://www.accuweather.com/en/ph/valenzuela/3424474/air-quality-index/3424474"}
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://www.google.com/"
-}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+           "Accept-Language": "en-US,en;q=0.9",
+           "Referer": "https://www.google.com/"}
 
 cities = {}
 print("Starting scrape...")
