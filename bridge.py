ea16ac90d502567fde87d6afe8ba354b5c3581e6
Benedict Caliba <benedict.caliba.sci@ust.edu.ph>
2026-05-09 22:41:08 +0800

    Refactor bridge.py for improved scraping logic

--- bridge.py
+++ bridge.py
@@ -42,15 +42,13 @@ def run_automation():
             "Taguig": "https://www.accuweather.com/en/ph/taguig/759349/air-quality-index/759349",
             "Valenzuela": "https://www.accuweather.com/en/ph/valenzuela/3424474/air-quality-index/3424474"}
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://www.google.com/"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
     cities = {}
 
     # Scrape
     for city, url in urls.items():
         response = requests.get(url, headers=headers)
-        time.sleep(3)
+        time.sleep(2)
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "html.parser")
             pols_idx_divs = soup.find_all("div", class_="pollutant-index")
